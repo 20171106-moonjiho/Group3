fdf544c436d9ab66a6dd9781e797cf7b857ef251
hss1502 <148041185+hss1502@users.noreply.github.com>
2023-10-16 15:52:57 +0900

    Add files via upload

---
+++ aviation/src/main/java/com/ming/boot/HomeController.java
@@ -0,0 +1,32 @@
+package com.ming.boot;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("index")
+	public void index() {}
+	
+	@RequestMapping("header")
+	public String header() {
+		return "default/header";
+	}
+	@RequestMapping("main")
+	public String main() {
+		return "default/main";
+	}
+	@RequestMapping("footer")
+	public String footer() {
+		return "default/footer";
+	}
+}
