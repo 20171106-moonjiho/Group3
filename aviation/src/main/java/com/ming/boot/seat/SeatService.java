66865f7b95aa1228a8efe480f558f24517283a51
hss1502 <148041185+hss1502@users.noreply.github.com>
2023-11-02 10:04:23 +0900

    예약 취소

--- aviation/src/main/java/com/ming/boot/seat/SeatService.java
+++ aviation/src/main/java/com/ming/boot/seat/SeatService.java
@@ -3,12 +3,17 @@ package com.ming.boot.seat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import com.ming.boot.member.IMemberMapper;
+import com.ming.boot.member.MemberDTO;
 
 import jakarta.servlet.http.HttpSession;
 
 @Service
 public class SeatService {
+	@Autowired private IMemberMapper m_mapper; 
 	@Autowired private SeatMapper mapper;
 	@Autowired private HttpSession session;
 
@@ -24,5 +29,25 @@ public class SeatService {
 
 	public List<String> getSeatByAirplane(String no) {
 		return mapper.getSeatByAirplane(no);
+	}
+
+	public String cancelProc(MemberDTO member, SeatDTO seat) {
+		if(member.getPw() == null || member.getPw().trim().isEmpty()) {
+			return "비밀번호를 입력하세요.";
+		}
+		if(member.getPw().equals(member.getConfirm()) == false) {
+			return "두 비밀번호를 일치하여 입력하세요.";
+		}
+		
+		MemberDTO check = m_mapper.login(member.getId());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		if(check != null && encoder.matches(member.getPw(), check.getPw()) == true) {
+			int result = mapper.cancelProc(seat);
+			if(result == 1)
+				return "예약 취소";
+			return "예약 취소를 다시 시도하세요.";
+		}
+		
+		return "아이디 또는 비밀번호를 확인 후 입력하세요";
 	}
 }
